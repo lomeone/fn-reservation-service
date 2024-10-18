af69d387e26cc08c05303d6427793c049b20fd9f
comstering <coms1101@gmail.com>
2024-10-18 23:27:48 +0900

    refactor: Refactor build.gradle.kts structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-val kotlin_version: String by project
+import java.io.ByteArrayOutputStream
+
+val group_name: String by project
+
 val logback_version: String by project
 val prometheus_version: String by project
 val swagger_version: String by project
@@ -9,30 +12,60 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
-group = "com.lomeone"
-version = "0.0.1"
+allprojects {
+    group = group_name
+    version = getGitHash()
 
-application {
-    mainClass.set("com.lomeone.ApplicationKt")
+    apply {
+        plugin("kotlin")
+        plugin("io.ktor.plugin")
+        plugin("org.jetbrains.kotlin.plugin.serialization")
+    }
 
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+    repositories {
+        mavenCentral()
+    }
 }
 
-repositories {
-    mavenCentral()
+subprojects {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    kotlin {
+        jvmToolchain(21)
+    }
+
+    dependencies {
+        // ktor
+        implementation("io.ktor:ktor-server-core-jvm")
+        implementation("io.ktor:ktor-server-resources-jvm")
+        implementation("io.ktor:ktor-server-call-logging-jvm")
+
+        // MongoDB
+        implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.1")
+
+        implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
+        implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
+        implementation("io.ktor:ktor-server-content-negotiation-jvm")
+        implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
+        implementation("io.ktor:ktor-server-netty-jvm")
+        implementation("ch.qos.logback:logback-classic:$logback_version")
+        testImplementation("io.ktor:ktor-server-test-host-jvm")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    }
 }
 
-dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-resources-jvm")
-    implementation("io.ktor:ktor-server-call-logging-jvm")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
-    implementation("io.micrometer:micrometer-registry-prometheus:$prometheus_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-test-host-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine = listOf("git", "rev-parse", "--short", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
 }
