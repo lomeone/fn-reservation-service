7c34596ae743080010860d564e2bae3cb0c7220d
comstering <coms1101@gmail.com>
2024-10-18 21:55:56 +0900

    refactor: Clean up dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 val kotlin_version: String by project
 val logback_version: String by project
 val prometheus_version: String by project
+val swagger_version: String by project
 
 plugins {
-    kotlin("jvm") version "2.0.21"
-    id("io.ktor.plugin") version "3.0.0"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    kotlin("jvm")
+    id("io.ktor.plugin")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 group = "com.lomeone"
@@ -24,9 +25,6 @@ repositories {
 
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-webjars-jvm")
-    implementation("org.webjars:jquery:3.2.1")
-    implementation("io.github.smiley4:ktor-swagger-ui:2.9.0")
     implementation("io.ktor:ktor-server-resources-jvm")
     implementation("io.ktor:ktor-server-call-logging-jvm")
     implementation("io.ktor:ktor-server-metrics-micrometer-jvm")
@@ -36,5 +34,5 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,17 @@
-rootProject.name = "fn-reservation-service"
+val service_name: String by settings
+rootProject.name = service_name
+
+pluginManagement {
+    val kotlin_version: String by settings
+    val ktor_version: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlin_version
+        id("io.ktor.plugin") version ktor_version
+        id("org.jetbrains.kotlin.plugin.serialization") version kotlin_version
+    }
+}
+
 include("domain")
 include("infrastructure")
 include("application")
