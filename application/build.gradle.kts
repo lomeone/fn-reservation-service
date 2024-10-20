23c25f37c62998db2e85975da6744a3940fd0194
comstering <coms1101@gmail.com>
2024-10-20 21:04:51 +0900

    feat: Add koin dependency

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,10 +1,27 @@
-dependencies {}
+val koin_version: String by project
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":infrastructure"))
+
+    implementation("io.insert-koin:koin-ktor:$koin_version")
+}
 
 application {
     mainClass.set("com.lomeone.application.ApplicationKt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+tasks.register<Copy>("mergeConfigs") {
+    from(project(":domain").file("src/main/resources/domain.conf"))
+    from(project(":infrastructure").file("src/main/resources/infrastructure.conf"))
+    into(layout.buildDirectory.dir("resources/main"))
+}
+
+tasks.named("processResources") {
+    dependsOn("mergeConfigs")
 }
 
 tasks.test {
