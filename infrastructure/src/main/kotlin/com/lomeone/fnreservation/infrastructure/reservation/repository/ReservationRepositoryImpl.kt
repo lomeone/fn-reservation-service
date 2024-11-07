80262f8467f87c4354cd5f93f4664a8eec97e7c0
comstering <coms1101@gmail.com>
2024-11-07 23:01:09 +0900

    feat: Implement save function for reserve repository using mongo db

--- infrastructure/src/main/kotlin/com/lomeone/fnreservation/infrastructure/reservation/repository/ReservationRepositoryImpl.kt
+++ infrastructure/src/main/kotlin/com/lomeone/fnreservation/infrastructure/reservation/repository/ReservationRepositoryImpl.kt
@@ -5,9 +5,13 @@ import com.lomeone.fnreservation.domain.reservation.repository.ReservationReposi
 import com.mongodb.MongoException
 import com.mongodb.client.model.Filters.and
 import com.mongodb.client.model.Filters.eq
+import com.mongodb.client.model.UpdateOptions
+import com.mongodb.client.model.Updates
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import kotlinx.coroutines.flow.firstOrNull
+import org.bson.conversions.Bson
 import org.slf4j.LoggerFactory
+import kotlin.reflect.full.memberProperties
 
 const val RESERVATION_COLLECTION = "reservation"
 
@@ -27,14 +31,37 @@ class ReservationRepositoryImpl(
 
     override suspend fun save(reservation: Reservation): Reservation {
         try {
-            val result = mongoDatabase.getCollection<Reservation>(RESERVATION_COLLECTION).insertOne(reservation)
+            if (isAlreadyExistReservation(reservation)) {
+                val query = eq("_id", reservation.id)
+                val updateList = mutableListOf<Bson>()
+                for(property in Reservation::class.memberProperties) {
+                    if (property.name != Reservation::id.name) {
+                        val value = property.get(reservation)
+                        if (value != null) {
+                            updateList.add(Updates.set(property.name, value))
+                        }
+                    }
+                }
 
-            return mongoDatabase.getCollection<Reservation>(RESERVATION_COLLECTION).withDocumentClass<Reservation>()
-            .find(eq("_id", result.insertedId))
-            .firstOrNull() ?: throw Exception()
+                val updates = Updates.combine(updateList)
+
+                val options = UpdateOptions().upsert(true)
+                return  mongoDatabase.getCollection<Reservation>(RESERVATION_COLLECTION).updateOne(query, updates, options).let { reservation }
+            } else {
+                val result = mongoDatabase.getCollection<Reservation>(RESERVATION_COLLECTION).insertOne(reservation)
+
+                return mongoDatabase.getCollection<Reservation>(RESERVATION_COLLECTION).withDocumentClass<Reservation>()
+                    .find(eq("_id", result.insertedId))
+                    .firstOrNull() ?: throw Exception()
+            }
         } catch (e: MongoException) {
             log.error("Unable to insert due to an error: $e")
             throw Exception()
         }
     }
+
+    private suspend fun isAlreadyExistReservation(reservation: Reservation) = mongoDatabase.getCollection<Reservation>(RESERVATION_COLLECTION)
+        .find(eq("_id", reservation.id))
+        .sort(org.bson.Document("createdAt", -1))
+        .firstOrNull() != null
 }
