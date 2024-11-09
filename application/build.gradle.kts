138494b2d4f605441ca27c59eeac9deca3c196e0
comstering <coms1101@gmail.com>
2024-11-09 19:25:26 +0900

    feat: Add jib config

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -1,6 +1,11 @@
+import java.io.ByteArrayOutputStream
+
 val koin_version: String by project
 val prometheus_version: String by project
 val swagger_version: String by project
+
+val image_registry: String by project
+val service_name: String by project
 
 plugins {
     id("io.ktor.plugin")
@@ -38,4 +43,54 @@ tasks.register<Copy>("mergeConfigs") {
 
 tasks.named("processResources") {
     dependsOn("mergeConfigs")
+}
+
+jib {
+    from {
+        image = "amazoncorretto:21"
+        platforms {
+            platform {
+                architecture = "amd64"
+                os = "linux"
+            }
+            platform {
+                architecture = "arm64"
+                os = "linux"
+            }
+        }
+    }
+    to {
+        image = "$image_registry/$service_name"
+        tags = getImageTags()
+    }
+}
+
+fun getImageTags(): Set<String> {
+    val tags = mutableSetOf<String>()
+    val branch = getGitCurrentBranch()
+
+    if (branch.isNotBlank()) {
+        tags.add(branch)
+    }
+    tags.add(getGitHash())
+
+    return tags
+}
+
+fun getGitCurrentBranch(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine = listOf("git", "rev-parse", "--abbrev-ref", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim().replace("/","-")
+}
+
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine = listOf("git", "rev-parse", "--short", "HEAD")
+        standardOutput = stdout
+    }
+    return stdout.toString().trim()
 }
