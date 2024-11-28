5dd0ac86f258c8d579a68100343c3823bb8f54ba
comstering <coms1101@gmail.com>
2024-11-28 23:09:34 +0900

    refactor Refactor using custom exception

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/reservation/service/ReserveService.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/reservation/service/ReserveService.kt
@@ -1,12 +1,14 @@
-package com.lomeone.com.lomeone.fnreservation.domain.reservation.service
+package com.lomeone.fnreservation.domain.reservation.service
 
+import com.lomeone.fnreservation.domain.reservation.exception.ReservationNotFoundException
 import com.lomeone.fnreservation.domain.reservation.repository.ReservationRepository
 
 class ReserveService(
     private val reservationRepository: ReservationRepository
 ) {
     suspend fun reserve(command: ReserveCommand): ReserveResult {
-        val reservation = reservationRepository.findByStoreBranchAndLatestGameType(command.storeBranch, command.gameType) ?: throw Exception("예약을 찾을 수 없습니다")
+        val reservation = reservationRepository.findByStoreBranchAndLatestGameType(command.storeBranch, command.gameType)
+            ?: throw ReservationNotFoundException(detail = mapOf("storeBranch" to command.storeBranch, "gameType" to command.gameType))
 
         if (reservation.isClosed()) {
             throw Exception("예약이 마감되었습니다")
