f6f0a92790043e3f1412781288ca5fcc7f0e91fe
comstering <comstering@gmail.com>
2025-01-27 03:00:25 +0900

    feat: Implement reservation notfound exception

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/reservation/exception/ReservationNotFoundException.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/reservation/exception/ReservationNotFoundException.kt
@@ -1,19 +1,22 @@
 package com.lomeone.fnreservation.domain.reservation.exception
 
+import com.lomeone.eunoia.exception.ErrorCode
+import com.lomeone.eunoia.exception.EunioaException
+import com.lomeone.eunoia.exception.ExceptionCategory
+import com.lomeone.eunoia.exception.ExceptionDetail
+
+private val ERROR_CODE = ErrorCode(
+    code = "user/deletion-request-not-found",
+    exceptionCategory = ExceptionCategory.NOT_FOUND
+)
+private const val MESSAGE = "Deletion request not found"
+
 class ReservationNotFoundException(
     detail: Map<String, Any>
-) : CustomException(
+) : EunioaException(
     message = MESSAGE,
     errorCode = ERROR_CODE,
     detail = ExceptionDetail(
         details = detail
     )
-) {
-    companion object {
-        val ERROR_CODE = ErrorCode(
-            code = "user/deletion-request-not-found",
-            exceptionCategory = ExceptionCategory.NOT_FOUND
-        )
-        const val MESSAGE = "Deletion request not found"
-    }
-}
+)
