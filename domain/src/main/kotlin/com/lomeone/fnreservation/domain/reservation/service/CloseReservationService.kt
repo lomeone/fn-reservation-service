ed801040e5f31bf0ffcb5a8582a4bd4dcf66734a
comstering <comstering@gmail.com>
2025-02-01 15:21:12 +0900

    refactor: Refactor to seperate function

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/reservation/service/CloseReservationService.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/reservation/service/CloseReservationService.kt
@@ -2,13 +2,14 @@ package com.lomeone.fnreservation.domain.reservation.service
 
 import com.lomeone.fnreservation.domain.reservation.entity.Reservation
 import com.lomeone.fnreservation.domain.reservation.exception.ReservationClosedException
+import com.lomeone.fnreservation.domain.reservation.exception.ReservationNotFoundException
 import com.lomeone.fnreservation.domain.reservation.repository.ReservationRepository
 
 class CloseReservationService(
     private val reservationRepository: ReservationRepository
 ) {
     fun closeReservation(command: CloseReservationCommand): CloseReservationResult {
-        val reservation = reservationRepository.findByStoreBranchAndLatestGameType(command.storeBranch, command.gameType) ?: throw Exception("예약을 찾을 수 없습니다")
+        val reservation = getReservation(command)
 
         ensureReservationOpened(reservation)
 
@@ -22,6 +23,9 @@ class CloseReservationService(
             session = savedReservation.session
         )
     }
+    private fun getReservation(command: CloseReservationCommand): Reservation =
+        reservationRepository.findByStoreBranchAndLatestGameType(command.storeBranch, command.gameType)
+            ?: throw ReservationNotFoundException(detail = mapOf("storeBranch" to command.storeBranch, "gameType" to command.gameType))
 
     private fun ensureReservationOpened(reservation: Reservation) {
         reservation.isClosed() && throw ReservationClosedException(
