5d2d9ba7893c17b548ccc3f406e8aae8b60db3ab
comstering <comstering@gmail.com>
2025-02-07 02:04:00 +0900

    feat: Implement MongoConfig

---
+++ infrastructure/src/main/kotlin/com/lomeone/fnreservation/infrastructure/database/mongodb/MongoConfig.kt
@@ -0,0 +1,30 @@
+package com.lomeone.fnreservation.infrastructure.database.mongodb
+
+import aws.sdk.kotlin.services.secretsmanager.model.GetSecretValueRequest
+import com.lomeone.fnreservation.infrastructure.aws.secretsmanager.SecretsManagerConfig
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoDatabase
+
+object MongoConfig {
+    private val mongoClient: MongoClient by lazy {
+        val uri = getMongodbUri()
+        MongoClient.create(uri)
+    }
+
+    fun getMongoDatabase(database: String): MongoDatabase = mongoClient.getDatabase(database)
+
+    private fun getMongodbUri(): String {
+        val valueRequest =
+            GetSecretValueRequest {
+                secretId = "mongodb-uri"
+            }
+
+        val response = SecretsManagerConfig.getSecretValue(valueRequest)
+
+        return response.secretString ?: throw Exception()
+    }
+
+    fun close() {
+        mongoClient.close()
+    }
+}
