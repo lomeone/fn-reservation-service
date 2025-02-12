e0fda639d704a15f8cc4fd97100b5ffcd2baa061
comstering <comstering@gmail.com>
2025-02-13 00:03:14 +0900

    feat: Add staff function for activate status

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/entity/Staff.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/entity/Staff.kt
@@ -2,15 +2,25 @@ package com.lomeone.fnreservation.domain.management.entity
 
 import org.bson.codecs.pojo.annotations.BsonId
 import org.bson.types.ObjectId
+import java.time.LocalDateTime
 
 data class Staff(
     @BsonId
     val id: ObjectId = ObjectId(),
     val storeBranch: String,
     val name: String,
-    val role: StaffRole = StaffRole.STAFF,
-    val status: StaffStatus = StaffStatus.ACTIVE
+    var role: StaffRole = StaffRole.STAFF,
+    var status: StaffStatus = StaffStatus.ACTIVE,
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now()
 ) {
+    fun inactivate() {
+        this.status = StaffStatus.INACTIVE
+    }
+
+    fun activate() {
+        this.status = StaffStatus.ACTIVE
+    }
 }
 
 enum class StaffRole {
--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/repository/StaffRepository.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/repository/StaffRepository.kt
@@ -3,6 +3,6 @@ package com.lomeone.fnreservation.domain.management.repository
 import com.lomeone.fnreservation.domain.management.entity.Staff
 
 interface StaffRepository {
-    fun findByStoreBranchAndName(storeBranch: String, name: String): Staff?
     fun save(staff: Staff): Staff
+    fun findByStoreBranchAndName(storeBranch: String, name: String): Staff?
 }
