e50268ffb7cedab00c6dc12ed43f21b530e6b2b1
comstering <comstering@gmail.com>
2025-03-08 22:20:55 +0900

    feat: Add interface repository for finding staffs by store branch

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/repository/StaffRepository.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/repository/StaffRepository.kt
@@ -4,5 +4,6 @@ import com.lomeone.fnreservation.domain.management.entity.Staff
 
 interface StaffRepository {
     fun save(staff: Staff): Staff
+    fun findByStoreBranch(storeBranch: String): List<Staff>
     fun findByStoreBranchAndName(storeBranch: String, name: String): Staff?
 }
