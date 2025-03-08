cb967e1e09383592c8b47451f4f8ba20016f5303
comstering <comstering@gmail.com>
2025-03-09 01:41:28 +0900

    feat: Add exception for staff not found

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/service/RegisterStaffService.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/service/RegisterStaffService.kt
@@ -1,6 +1,6 @@
 package com.lomeone.fnreservation.domain.management.service
 
-import com.lomeone.fnreservation.domain.management.ecxeption.AlreadyExistStaffException
+import com.lomeone.fnreservation.domain.management.exception.AlreadyExistStaffException
 import com.lomeone.fnreservation.domain.management.entity.Staff
 import com.lomeone.fnreservation.domain.management.repository.StaffRepository
 
