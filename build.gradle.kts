85e6e7518e8dc057ffdcdf3a1ba1fbbbe5ad395f
comstering <comstering@gmail.com>
2025-03-12 23:47:58 +0900

    feat: Add spring boot dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,9 @@ val eunoia_exception_version: String by project
 
 plugins {
     kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
     id("org.jetbrains.kotlin.plugin.serialization")
 }
 
@@ -16,6 +19,9 @@ allprojects {
 
     apply {
         plugin("kotlin")
+        plugin("kotlin-spring")
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
         plugin("org.jetbrains.kotlin.plugin.serialization")
     }
 
@@ -46,6 +52,8 @@ subprojects {
     }
 
     dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-validation")
+
         // MongoDB
         implementation("org.mongodb:mongodb-driver-kotlin-coroutine:4.10.1")
 
@@ -54,6 +62,7 @@ subprojects {
         implementation("com.lomeone.eunoia:exception:$eunoia_exception_version")
 
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 
     tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,16 @@ rootProject.name = service_name
 
 pluginManagement {
     val kotlin_version: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagement: String by settings
     val ktor_version: String by settings
 
     plugins {
         kotlin("jvm") version kotlin_version
+        kotlin("plugin.spring") version kotlin_version
         id("io.ktor.plugin") version ktor_version
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagement
         id("org.jetbrains.kotlin.plugin.serialization") version kotlin_version
     }
 }
