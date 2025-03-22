30736ce9ee5c5f0f00e84aefefaa657f36bf5074
comstering <comstering@gmail.com>
2025-03-22 23:17:06 +0900

    refactor: Refactor staff entity

--- domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/repository/StaffRepository.kt
+++ domain/src/main/kotlin/com/lomeone/fnreservation/domain/management/repository/StaffRepository.kt
@@ -5,6 +5,7 @@ import com.lomeone.fnreservation.domain.management.entity.StaffStatus
 
 interface StaffRepository {
     fun save(staff: Staff): Staff
+    fun findById(id: String): Staff?
     fun findByStoreBranchAndStatus(storeBranch: String, status: StaffStatus): List<Staff>
     fun findByStoreBranchAndName(storeBranch: String, name: String): Staff?
 }
