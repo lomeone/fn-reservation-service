8c64279647f85d9543f206b8dbd9c4143a4ca0c7
comstering <comstering@gmail.com>
2025-03-24 01:50:18 +0900

    refactor: Remove println

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -62,14 +62,10 @@ fun getImageTags(): Set<String> {
     val tags = mutableSetOf<String>()
     val branch = getGitCurrentBranch()
 
-    println("branch: $branch")
-
     if (branch.isNotBlank()) {
         tags.add(branch)
     }
     tags.add(getGitHash())
-
-    println("tags: $tags")
 
     return tags
 }
